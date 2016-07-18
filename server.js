7777ad3185b200bedf841695191efcfd9ae85557
deivis <deivis.wingert@labtrans.ufsc.br>
2016-07-18 18:57:33 -0300

    General: added some basic and configuration files

---
+++ server.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config');
+
+new WebpackDevServer(webpack(config), {
+  hot: true,
+  stats: {
+    colors: true
+  }
+}).listen(3000, 'localhost', function (err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Listening at localhost:3000');
+});
